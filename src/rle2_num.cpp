d3034514ec7a253aef8bb4e31fee838d1edcd304
AJ Smit <albertus.smit@gmail.com>
2018-05-30 11:47:58 +0200

    Work on proto_gap function in C++.

--- src/rle2_num.cpp
+++ src/rle2_num.cpp
@@ -19,26 +19,32 @@ NumericMatrix rle2_num(NumericVector x) {
   int rows = 1;
 
   for (int a = 1; a < n; ++a)
-    if (x(a)!=x(a-1)) rows += 1;
-    NumericMatrix out(rows,4);
+
+    if (x(a) != x(a - 1)) rows += 1;
+
+    NumericMatrix out(rows, 4);
+
     double presval = x(0);
     int prespos = 0;
     int index = -1;
-      for (int b = 1; b < n; ++b) {
-        if (x(b)!=presval) {
-          index += 1;
-          out(index,0) = presval;
-          out(index,1) = prespos+1;
-          out(index,2) = b;
-          out(index,3) = b-prespos;
-          presval = x(b);
-          prespos = b;
-          }
+
+    for (int b = 1; b < n; ++b) {
+      if (x(b) != presval) {
+        index += 1;
+        out(index, 0) = presval;
+        out(index, 1) = prespos + 1;
+        out(index, 2) = b;
+        out(index, 3) = b - prespos;
+        presval = x(b);
+        prespos = b;
         }
-      index += 1;
-      out(index,0) = presval;
-      out(index,1) = prespos+1;
-      out(index,2) = n;
-      out(index,3) = n-prespos;
-      return(out);
       }
+
+    index += 1;
+    out(index, 0) = presval;
+    out(index, 1) = prespos + 1;
+    out(index, 2) = n;
+    out(index, 3) = n - prespos;
+    return(out);
+
+    }
--- src/rle2_proto.cpp
+++ src/rle2_proto.cpp
@@ -11,34 +11,61 @@ using namespace Rcpp;
 //      column 3: end index of the run of values in column 1
 //      column 4: length of the run of values in column 1
 
+// declare replication function
+NumericVector rep_vec(const NumericVector& x, const IntegerVector& times) {
+  std::size_t n = times.size();
+  if (n != 1 && n != x.size())
+    stop("Invalid 'times' value");
+  std::size_t n_out = std::accumulate(times.begin(), times.end(), 0);
+  NumericVector res = no_init(n_out);
+  auto begin = res.begin();
+  for (std::size_t i = 0, ind = 0; i < n; ind += times[i], ++i) {
+    auto start = begin + ind;
+    auto end = start + times[i];
+    std::fill(start, end, x[i]);
+  }
+  return res;
+}
+
 // [[Rcpp::export]]
-NumericMatrix rle2_num(NumericVector x) {
+List rle2_proto(NumericVector x) {
 
   int n = x.size();
-
   int rows = 1;
 
   for (int a = 1; a < n; ++a)
-    if (x(a)!=x(a-1)) rows += 1;
-    NumericMatrix out(rows,4);
+
+    if (x(a) != x(a - 1)) rows += 1;
+
+    NumericMatrix out(rows, 5);
     double presval = x(0);
     int prespos = 0;
     int index = -1;
-      for (int b = 1; b < n; ++b) {
-        if (x(b)!=presval) {
-          index += 1;
-          out(index,0) = presval;
-          out(index,1) = prespos+1;
-          out(index,2) = b;
-          out(index,3) = b-prespos;
-          presval = x(b);
-          prespos = b;
-          }
-        }
-      index += 1;
-      out(index,0) = presval;
-      out(index,1) = prespos+1;
-      out(index,2) = n;
-      out(index,3) = n-prespos;
-      return(out);
+    int counter = 0;
+
+    for (int b = 1; b < n; ++b) {
+      if (x(b) != presval) {
+        index += 1;
+        out(index, 0) = index + 1;    // run number
+        out(index, 1) = presval;      // equal to R rle $values
+        out(index, 2) = prespos + 1;
+        out(index, 3) = b;
+        out(index, 4) = b - prespos;  // equal to R rle $lengths
+        presval = x(b);
+        prespos = b;
+      }
     }
+
+    NumericVector repl_vec = out(_, 0);
+    NumericVector _times_vec = out(_, 4);
+    IntegerVector times_vec = as<IntegerVector>(_times_vec);
+
+    NumericVector res(0);
+    res = rep_vec(repl_vec, times_vec);
+
+    List ret;
+    ret["rle"] = out;
+    ret["res"] = res;
+    return ret;
+
+}
